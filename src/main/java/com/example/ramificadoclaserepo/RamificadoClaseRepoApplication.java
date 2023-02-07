8a6383a81b2fe874c6eb2b9399bfeba633cf56bb
harnina2017 <36604786+harnina2017@users.noreply.github.com>
2023-02-07 09:59:40 +0100

    inicial develop

---
+++ src/main/java/com/example/ramificadoclaserepo/RamificadoClaseRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ramificadoclaserepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamificadoClaseRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamificadoClaseRepoApplication.class, args);
+    }
+
+}
